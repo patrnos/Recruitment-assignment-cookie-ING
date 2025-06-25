d5933b3fbab10cd1843d5a9b26a4dba73a5c2f6b
Patryk Nosal <pnosal@vanto.pl>
2025-06-25 16:24:30 +0200

    13 la t

--- tests/test_cookie_consent.py
+++ tests/test_cookie_consent.py
@@ -5,7 +5,7 @@ def test_scenario_with_analytics_consent(page: Page):
     page.goto("https://www.ing.pl/")
 
     # Sprawdzenie i próba kliknięcia "Dostosuj" (tylko tutaj if/try)
-    page.get_by_role("checkbox", name="hCaptcha checkbox with text '").click()
+    page.get_by_text("Request unsuccessful.").content_frame.locator("iframe[title=\"Widget containing checkbox for hCaptcha security challenge\"]").content_frame.get_by_role("checkbox", name="hCaptcha checkbox with text '").click()
 
     # Dalsza część testu – bez żadnych if/try/except
     page.get_by_role("button", name="Dostosuj").click()
